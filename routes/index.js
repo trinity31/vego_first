1333bb8080619401d45dec3d3775e1844dd84be1
Trinity Song <kyeongyeon.song@gmail.com>
2016-07-31 12:52:00 +0900

    fix route error

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+*/
+
+router.get('/', function(req, res, next) {
+  var drinks = [
+    { name: 'Bloody Mary', drunkness:3 },
+    { name: 'Martini', drunkness: 5 },
+    { name: 'Scotch', drunkness: 10 }
+  ];
+
+  var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+  res.render('index', {
+    drinks: drinks,
+    tagline: tagline
+  });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
 
 module.exports = router;
