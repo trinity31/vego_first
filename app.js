a56dab0f061c3a100646c036c631d8bd9554c817
Trinity Song <kyeongyeon.song@gmail.com>
2016-07-31 16:48:54 +0900

    change db params

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-db.sequelize.sync({force:true}).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log('Express listening on port ' + PORT + '!');
   }); 
